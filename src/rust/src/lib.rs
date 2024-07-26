ae92730d9d717a7c92b120441a5705467e876cc8
simonpcouch <simonpatrickcouch@gmail.com>
2024-07-26 11:37:46 -0500

    remove dead code

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -4,26 +4,6 @@ mod linear;
 mod trees;
 
 use extendr_api::prelude::*;
-use std::ops::Deref;
-
-// Define a wrapper struct that holds the value and implements Deref
-struct DeferredDeref<T>(Box<T>);
-
-impl<T> DeferredDeref<T> {
-    // Constructor function to create a DeferredDeref instance
-    fn new(value: T) -> Self {
-        DeferredDeref(Box::new(value))
-    }
-}
-
-// Implement Deref for DeferredDeref
-impl<T> Deref for DeferredDeref<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
 
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
